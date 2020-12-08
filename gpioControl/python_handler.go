d48b6367ae86c792dac485742bb208d797fbc9f2
chinux_2012 <1872326010@qq.com>
2020-12-08 18:35:13 +0800

    new file and rename

---
+++ gpioControl/python_handler.go
@@ -0,0 +1,37 @@
+package gpioControl
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	// "encoding/json"
+)
+
+func ExecPython(params []string) (string, error) {
+	var args = append([]string{}, params...)
+	out, err := exec.Command("python3", args...).Output()
+	if err != nil {
+		fmt.Println("exec python err =>", err.Error())
+		return "", err
+	}
+
+	// out_emp_ind := bytes.IndexByte(out, 0)
+
+	// if out_emp_ind < 0 {
+	// 	out_emp_ind = out_emp_ind
+	// }
+	// out = out[:out_emp_ind]
+
+	outData := string(out)
+
+	reg := regexp.MustCompile("\\s+")
+	outData = reg.ReplaceAllString(outData, "")
+
+	return outData, nil
+}
+
+func PyGpioExec(params ...string) (string, error) {
+    tmpParams := []string{"gpio.py"}
+    tmpParams = append(tmpParams, params...)
+    return ExecPython(tmpParams)
+}
